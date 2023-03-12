708e431f67a7f13be147ba04d34b8f57da47e7d5
Mikrokosmos1306 <127518895+Mikrokosmos1306@users.noreply.github.com>
2023-03-12 23:20:32 +0700

    Add files via upload

---
+++ BTLyThuy_Bai7_Trang70.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTapLyThuyetSo2
+{
+    public partial class BaiTapTrang35 : Form
+    {
+        public BaiTapTrang35()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
