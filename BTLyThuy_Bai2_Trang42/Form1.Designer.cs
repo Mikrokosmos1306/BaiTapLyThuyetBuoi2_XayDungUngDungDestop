6c821f31d6f32b861aa74632f83c04ad611fc607
Mikrokosmos1306 <127518895+Mikrokosmos1306@users.noreply.github.com>
2023-03-12 23:18:48 +0700

    Add files via upload

---
+++ BTLyThuy_Bai2_Trang42/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BTLyThuy_Bai2_Trang42
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblUser = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblUser
+            // 
+            this.lblUser.AutoSize = true;
+            this.lblUser.Location = new System.Drawing.Point(105, 74);
+            this.lblUser.Name = "lblUser";
+            this.lblUser.Size = new System.Drawing.Size(43, 20);
+            this.lblUser.TabIndex = 0;
+            this.lblUser.Text = "User";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(105, 131);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(78, 20);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(105, 183);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(89, 20);
+            this.lblDescription.TabIndex = 2;
+            this.lblDescription.Text = "Description";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(238, 68);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(176, 26);
+            this.txtUser.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 222);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(583, 191);
+            this.textBox2.TabIndex = 4;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(238, 128);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(176, 26);
+            this.txtPassword.TabIndex = 5;
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(229, 439);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(96, 31);
+            this.btnOK.TabIndex = 6;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(396, 439);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(93, 31);
+            this.btnCancel.TabIndex = 7;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 498);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblUser);
+            this.Name = "Form1";
+            this.Text = "Submit";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblUser;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.Button btnCancel;
+    }
+}
+
---
+++ BTLyThuy_Bai4_Trang52/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BTLyThuy_Bai4_Trang52
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lbDanhsach = new System.Windows.Forms.ListBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.lbDanhsachchon = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(176, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Danh sách sinh viên lớp";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(478, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(278, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Danh sách sinh viên tham gia bóng đá";
+            // 
+            // lbDanhsach
+            // 
+            this.lbDanhsach.FormattingEnabled = true;
+            this.lbDanhsach.ItemHeight = 20;
+            this.lbDanhsach.Items.AddRange(new object[] {
+            "Pham Vu Ngoc Ha",
+            "Hoang Van Hung",
+            "Le Thi Van",
+            "Nguyen Thi Thu",
+            "Le Van Quoc",
+            "Le Van Phuoc",
+            "Truong Van Dung"});
+            this.lbDanhsach.Location = new System.Drawing.Point(35, 129);
+            this.lbDanhsach.Name = "lbDanhsach";
+            this.lbDanhsach.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbDanhsach.Size = new System.Drawing.Size(300, 304);
+            this.lbDanhsach.TabIndex = 2;
+            this.lbDanhsach.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(356, 178);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(81, 43);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Chọn";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Location = new System.Drawing.Point(356, 270);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(81, 42);
+            this.btnXoa.TabIndex = 5;
+            this.btnXoa.Text = "Xoá";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // lbDanhsachchon
+            // 
+            this.lbDanhsachchon.FormattingEnabled = true;
+            this.lbDanhsachchon.ItemHeight = 20;
+            this.lbDanhsachchon.Location = new System.Drawing.Point(482, 129);
+            this.lbDanhsachchon.Name = "lbDanhsachchon";
+            this.lbDanhsachchon.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lbDanhsachchon.Size = new System.Drawing.Size(300, 304);
+            this.lbDanhsachchon.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 510);
+            this.Controls.Add(this.lbDanhsachchon);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lbDanhsach);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox lbDanhsach;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.ListBox lbDanhsachchon;
+    }
+}
+
